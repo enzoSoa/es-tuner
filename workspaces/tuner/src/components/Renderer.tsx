f6c30348a1b452f98a5f9cf3f68acb7b5470ee4a
Enzo Soares <enzoayrsoares@gmail.com>
2023-03-25 11:20:37 +0100

    :sparkles: adding instrument component it load a model from public folder using its name + using a props to set x position

--- workspaces/tuner/src/components/Renderer.tsx
+++ workspaces/tuner/src/components/Renderer.tsx
@@ -1,10 +1,21 @@
-import {Canvas} from "@react-three/fiber";
+import {Canvas, Camera} from "@react-three/fiber";
+import {Instrument} from "./Instrument";
+import {PerspectiveCamera, Vector3} from "three";
+import {useEffect, useState} from "react";
 
 export function Renderer() {
-  return <Canvas>
-    <mesh>
-      <boxGeometry />
-      <meshStandardMaterial />
-    </mesh>
+  const [camera, setCamera] = useState<Camera | undefined>(undefined);
+
+  useEffect(() => {
+    const newCamera = new PerspectiveCamera(60, window.innerHeight / window.innerWidth, 0.01, 4000);
+    newCamera.lookAt(new Vector3(0, 0, 0));
+    newCamera.position.set(0, 0, 2);
+
+    setCamera(newCamera);
+  },[])
+
+  return <Canvas style={{height: '100vh', width: '100vw'}} camera={camera}>
+    <ambientLight intensity={1}/>
+    <Instrument name={'guitar'} position={0}/>
   </Canvas>;
 }
