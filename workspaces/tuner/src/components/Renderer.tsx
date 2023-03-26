f6baec5a4df056443ed1c1c3176a6510a92b4bd5
Enzo Soares <enzoayrsoares@gmail.com>
2023-03-26 17:43:37 +0200

    :sparkles: adding slider scrolling

--- workspaces/tuner/src/components/Renderer.tsx
+++ workspaces/tuner/src/components/Renderer.tsx
@@ -1,23 +1,66 @@
-import {Canvas, Camera} from "@react-three/fiber";
+import {Canvas} from "@react-three/fiber";
 import {Instrument} from "./Instrument";
 import {PerspectiveCamera, Vector3} from "three";
-import {useEffect, useState} from "react";
+import {MouseEventHandler, useEffect, useState} from "react";
 
 export function Renderer() {
-  const [camera, setCamera] = useState<Camera | undefined>(undefined);
+  const [aimedPosX, setAimedPosX] = useState(0);
+  const [posX, setPosX] = useState(0);
+
+  const camera = new PerspectiveCamera(2, window.innerHeight / window.innerWidth, 1, 40000000);
+  camera.position.set(posX, 0, 50);
+  camera.lookAt(new Vector3(posX, 0, 0));
+
+  const instrumentsGap = 0.5;
+  const instruments = ['guitar', 'guitar', 'guitar', 'guitar', 'guitar']
+
+  const [isScrolling, setIsScrolling] = useState(false);
+  const [initialMousePosition, setInitialMousePosition] = useState(0);
+  const [scrollLeft, setScrollLeft] = useState(0);
 
   useEffect(() => {
-    const newCamera = new PerspectiveCamera(2, window.innerHeight / window.innerWidth, 1, 40000000);
-    newCamera.lookAt(new Vector3(0, 0, 0));
-    newCamera.position.set(0, 0, 50);
+    if (posX === aimedPosX) return;
+    else if (posX >= aimedPosX - 0.001 && posX <= aimedPosX + 0.001) setPosX(aimedPosX);
+    else setPosX((aimedPosX - posX) * 0.0003 + posX);
+  }, [aimedPosX,posX]);
 
-    setCamera(newCamera);
-  },[]);
-  
-  const instrumentsList = ['guitar', 'guitar', 'guitar', 'guitar', 'guitar']
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      const mouseXMove = initialMousePosition - event.screenX;
+      const mousePercentage = mouseXMove / window.innerWidth * 2;
 
-  return <Canvas style={{height: '100vh', width: '100vw'}} camera={camera}>
+      const maxScroll = (instruments.length - 1) * instrumentsGap;
+      const movement = mousePercentage * maxScroll + scrollLeft;
+
+      setAimedPosX(Math.round(Math.min(Math.max(movement, 0), maxScroll) * 2) / 2);
+    };
+
+    if (isScrolling) {
+      document.addEventListener('mousemove', handleMouseMove);
+    } else {
+      document.removeEventListener('mousemove', handleMouseMove);
+    }
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, [initialMousePosition, isScrolling, scrollLeft]);
+
+  const handleMouseDown: MouseEventHandler = (event) => {
+    setIsScrolling(true);
+    setInitialMousePosition(event.nativeEvent.screenX);
+    setScrollLeft(posX);
+  };
+  const handleMouseUp = () => setIsScrolling(false);
+
+
+  return <Canvas
+    style={{height: '100vh', width: '100vw', cursor: 'grab'}}
+    camera={camera}
+    onMouseDown={handleMouseDown}
+    onMouseUp={handleMouseUp}
+  >
     <ambientLight intensity={1}/>
-    {instrumentsList.map((instrument, index) => <Instrument key={`instrument-${index}`} name={'guitar'} position={0.5 * index}/>)}
+    {instruments.map((instrument, index) => <Instrument key={`instrument-${index}`} name={'guitar'}
+                                                        position={instrumentsGap * index}/>)}
   </Canvas>;
 }
