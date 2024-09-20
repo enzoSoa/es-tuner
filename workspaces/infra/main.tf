289a5240fd348a69ca240c26a7dd8d3e097c95f9
enzoSoa <enzoayrsoares@gmail.com>
2024-09-20 22:50:00 +0200

    Adding frontend s3 in terraform conf

--- workspaces/infra/main.tf
+++ workspaces/infra/main.tf
@@ -1,25 +1,46 @@
 terraform {
   required_providers {
-	docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.0.1"
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.68"
     }
   }
 }
 
-provider "docker" {}
-
-resource "docker_image" "nginx" {
-  name         = "nginx"
-  keep_locally = false
+provider "aws" {
+  region = "eu-west-1" 
 }
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name  = "tutorial"
+resource "aws_s3_bucket" "bucket" {
+  bucket = "es-tuner"
+  force_destroy = true
+}
 
-  ports {
-    internal = 80
-    external = 8000
+resource "aws_s3_bucket_ownership_controls" "bucket_ownership_control" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
   }
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.bucket_ownership_control]
+  bucket = aws_s3_bucket.bucket.id
+  acl = "private"
+}
+
+resource "aws_s3_object" "root_bucket_object" {
+    for_each        = fileset("../frontend/dist/", "*")
+    bucket          = aws_s3_bucket.bucket.id
+    key             = each.value
+    source          = "../frontend/dist/${each.value}"
+    etag            = filemd5("../frontend/dist/${each.value}")
+}
+
+resource "aws_s3_object" "assets_bucket_object" {
+    for_each        = fileset("../frontend/dist/assets/", "*")
+    bucket          = aws_s3_bucket.bucket.id
+    key             = "assets/${each.value}"
+    source          = "../frontend/dist/assets/${each.value}"
+    etag            = filemd5("../frontend/dist/assets/${each.value}")
 }
